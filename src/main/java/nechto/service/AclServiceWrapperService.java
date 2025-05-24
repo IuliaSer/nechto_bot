eac961c1af559207c9fbebd9028d6aaea456c499
iulia_serzantova <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>
2025-05-24 18:36:30 +0300

    delete spring security files

--- src/main/java/nechto/service/AclServiceWrapperService.java
+++
@@ -1,8 +0,0 @@
-package nechto.service;
-
-import org.springframework.security.acls.model.Permission;
-
-public interface AclServiceWrapperService {
-
-    void createPermission(Object object, Permission permission);
-}
